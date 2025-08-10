f812f9520535d90a479683b7ec97e1c5e300d6d6
Hanaa Sadoqi <hsadoqi@gmail.com>
2025-08-10 02:40:23 -0400

    fix: update runtime for twitter og image

--- src/app/twitter-image.tsx
+++ src/app/twitter-image.tsx
@@ -5,7 +5,7 @@ export const size = { width: 1200, height: 630 };
 export const contentType = "image/png";
 export const alt = "Synapcity";
 
-export const runtime = "edge";
+export const runtime = 'nodejs';
 
 export default function Image({
   _params,
