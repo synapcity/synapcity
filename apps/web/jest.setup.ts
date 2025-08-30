8d38bb9ac10f9b2cc1d6abca666534e262335697
Hanaa Sadoqi <hsadoqi@gmail.com>
2025-08-29 20:20:50 -0400

    refactor: add scaffold files to pnpm app; update workflows; add polyfills to jest setup

--- apps/web/jest.setup.ts
+++ apps/web/jest.setup.ts
@@ -13,6 +13,29 @@ global.ResizeObserver = ResizeObserver;
 import "jest-axe/extend-expect";
 import { cleanup } from "@testing-library/react";
 
+// Polyfill TextEncoder/TextDecoder for Node test environment
+// Some dependencies (e.g., iobuffer/fast-png/jspdf) expect these globals.
+try {
+  // Prefer Node's util implementation to avoid jsdom inconsistencies
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { TextEncoder, TextDecoder } = require("node:util");
+  if (!global.TextEncoder) global.TextEncoder = TextEncoder;
+  if (!global.TextDecoder) global.TextDecoder = TextDecoder;
+} catch (_) {
+  // no-op if not available; tests that need it will fail clearly
+}
+
+// Polyfill Web Crypto if missing (getRandomValues, subtle, etc.)
+try {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { webcrypto } = require("node:crypto");
+  if (!global.crypto || !global.crypto.getRandomValues) {
+    global.crypto = webcrypto;
+  }
+} catch (_) {
+  // ignore if not available
+}
+
 afterEach(() => {
   cleanup();
   jest.clearAllMocks();
